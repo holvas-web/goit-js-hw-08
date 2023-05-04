c06ee64e1d512dabe1e3fc0feeaa91a8ad9e7906
Vasyl Holubovskyi <66141432+holvas-web@users.noreply.github.com>
2023-05-04 16:08:43 +0200

    upgrade hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,50 +1,34 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
 
-const gallery = document.querySelector('.gallery')
-const items = [];
+const gallery = document.querySelector('.gallery');
+const markupItems = createGalleryMarkup(galleryItems);
+gallery.insertAdjacentHTML('beforeend', markupItems);
 
-galleryItems.forEach(element => {
-	const galleryItem = document.createElement('div')
-	galleryItem.className = 'gallery__item'
-	const galleryLink = document.createElement('a')
-	galleryLink.className = 'gallery__link'
-	galleryLink.href = element.original
-	const galleryImage = document.createElement('img')
-    galleryImage.className = 'gallery__image'
-    galleryImage.src = element.preview;
-    galleryImage.setAttribute('data-source', element.original)
-    galleryImage.alt = element.description;
+// візуалізація елементів
+function createGalleryMarkup(items) { 
+    return items.map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+        </a>
+    </li>`
+}).join('');
+}
+gallery.style = 'list-style: none';
 
-	galleryItem.append(galleryLink)
-	galleryLink.append(galleryImage)
-	items.push(galleryItem)
-})
-
-gallery.append(...items)
-
-gallery.addEventListener('click', e => {
-    e.preventDefault();
-    if (e.target.nodeName !== 'IMG') {
-		return
-	}
-
-    const selectedImage = e.target.getAttribute('data-source')
-
-    const instance = basicLightbox.create(`
-    <img src="${selectedImage}" width="800" height="600">
-`)
-
-    instance.show()
-    
-    gallery.addEventListener('keydown', e => {
-		if (e.key === 'Escape') {
-			instance.close()
-		}
-	})
-})
+// використання бібліотеки SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', 
+    captionDelay: 250,
+});
